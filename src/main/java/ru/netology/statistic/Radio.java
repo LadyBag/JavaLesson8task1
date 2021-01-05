05cf48f8773370ccaa820fd10f3add7059f9688e
Sergei Ovchinnikov <s.y.ovchinnikov@anoitr.ru>
2021-01-05 16:03:57 +0300

    добавила радио

--- src/main/java/ru/netology/statistic/Radio.java
+++ src/main/java/ru/netology/statistic/Radio.java
@@ -1,2 +1,55 @@
-package ru.netology.statistic;public class Radio {
+package ru.netology.statistic;
+
+public class Radio {
+    private int currentStationNumber;
+    private int soundVolume;
+    private int minVolume = 0;
+    private int maxVolume = 10;
+    private int minStationNumber = 0;
+    private int maxStationNumber = 9;
+
+
+    public void next() {
+        if (currentStationNumber == maxStationNumber) {
+            currentStationNumber = minStationNumber;
+        } else {
+            currentStationNumber = currentStationNumber + 1;
+        }
+    }
+
+    public void prev() {
+        if (currentStationNumber == minStationNumber) {
+            currentStationNumber = maxStationNumber;
+        } else {
+            currentStationNumber = currentStationNumber - 1;
+        }
+    }
+
+    public int getCurrentStationNumber() {
+        return currentStationNumber;
+    }
+
+    public int getSoundVolume() {
+        return soundVolume;
+    }
+
+    public void setSoundVolume(int soundVolume) {
+        this.soundVolume = soundVolume;
+    }
+
+    public void setCurrentStationNumber(int currentStationNumber) {
+        this.currentStationNumber = currentStationNumber;
+    }
+
+    public void plusVolume() {
+        if (soundVolume != maxVolume) {
+            soundVolume = soundVolume + 1;
+        }
+    }
+
+    public void minusVolume() {
+        if (soundVolume != minVolume) {
+            soundVolume = soundVolume - 1;
+        }
+    }
 }
--- src/test/java/ru/netology/statistic/RadioTest.java
+++ src/test/java/ru/netology/statistic/RadioTest.java
@@ -1,4 +1,98 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class RadioTest {
-  
+
+    @Test
+    void next() {
+        Radio radio = new Radio();
+        int expected = 1;
+
+        radio.next();
+
+        assertEquals(expected, radio.getCurrentStationNumber());
+    }
+
+    @Test
+    void nextMax() {
+        Radio radio = new Radio();
+        int expected = 0;
+
+        radio.setCurrentStationNumber(9);
+        radio.next();
+
+        assertEquals(expected, radio.getCurrentStationNumber());
+    }
+
+    @Test
+    void prevMin() {
+        Radio radio = new Radio();
+        int expected = 9;
+
+        radio.setCurrentStationNumber(0);
+        radio.prev();
+
+        assertEquals(expected, radio.getCurrentStationNumber());
+    }
+
+    @Test
+    void prev() {
+        Radio radio = new Radio();
+        int expected = 2;
+
+        radio.setCurrentStationNumber(3);
+        radio.prev();
+
+        assertEquals(expected, radio.getCurrentStationNumber());
+    }
+
+
+
+    @Test
+    void plusVolume() {
+        Radio radio = new Radio();
+        int expected = 10;
+
+        radio.setSoundVolume(9);
+        radio.plusVolume();
+
+        assertEquals(expected, radio.getSoundVolume());
+    }
+
+    @Test
+    void plusVolumeMax() {
+        Radio radio = new Radio();
+        int expected = 10;
+
+        radio.setSoundVolume(10);
+        radio.plusVolume();
+
+        assertEquals(expected, radio.getSoundVolume());
+    }
+
+
+    @Test
+    void minusVolume() {
+        Radio radio = new Radio();
+        int expected = 1;
+
+        radio.setSoundVolume(2);
+        radio.minusVolume();
+
+        assertEquals(expected, radio.getSoundVolume());
+    }
+
+    @Test
+    void minusVolumeMin() {
+        Radio radio = new Radio();
+        int expected = 0;
+
+        radio.setSoundVolume(0);
+        radio.minusVolume();
+
+        assertEquals(expected, radio.getSoundVolume());
+    }
 }
